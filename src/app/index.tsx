77b63f6bf37afc8c256b7f124097f64741ebc8be
Kissuki <andhika.smandading@gmail.com>
2024-03-06 21:27:01 +0700

    create cubic animation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,17 @@
+import React from "react";
 import { Dimensions, StyleSheet, Text, View } from "react-native";
-import Carousel from "react-native-reanimated-carousel";
+import Animated, {
+  Extrapolation,
+  interpolate,
+  interpolateColor,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+import Carousel, {
+  type TAnimationStyle,
+} from "react-native-reanimated-carousel";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+import { withAnchorPoint } from "@/utils/anchor-point";
 
 const colors = ["#FF64d4", "#FFe142", "#42c6ff"];
 const weathers = [
@@ -17,40 +28,133 @@ const weathers = [
 ];
 
 const width = Dimensions.get("screen").width;
+const height = Dimensions.get("screen").height;
 
 export default function MainScreen() {
   const insets = useSafeAreaInsets();
 
+  const PAGE_WIDTH = width;
+  const PAGE_HEIGHT = height;
+
+  const animationStyle: TAnimationStyle = React.useCallback(
+    (value: number) => {
+      "worklet";
+      const zIndex = interpolate(value, [-1, 0, 1], [-1000, 0, -1000]);
+
+      const translateX = interpolate(
+        value,
+        [-1, 0, 1],
+        [-PAGE_WIDTH * 1.5, 0, PAGE_WIDTH * 1.5],
+        Extrapolation.CLAMP
+      );
+
+      const scale = interpolate(
+        value,
+        [-1, 0, 1],
+        [0.49, 1, 0.49],
+        Extrapolation.CLAMP
+      );
+
+      const perspective = interpolate(
+        value,
+        [-1, 0, 1],
+        [PAGE_WIDTH * 0.89, PAGE_WIDTH * 1.5, PAGE_WIDTH * 0.89],
+        Extrapolation.CLAMP
+      );
+
+      const rotateY = `${interpolate(
+        value,
+        [-1, 0, 1],
+        [-135, 0, 135],
+        Extrapolation.CLAMP
+      )}deg`;
+
+      const transform = {
+        transform: [{ scale }, { translateX }, { perspective }, { rotateY }],
+      };
+
+      return {
+        ...withAnchorPoint(
+          transform,
+          { x: 0.5, y: 0.5 },
+          { width: PAGE_WIDTH, height: PAGE_HEIGHT }
+        ),
+        zIndex,
+      };
+    },
+    [PAGE_HEIGHT, PAGE_WIDTH]
+  );
+
   return (
-    <View style={[styles.container, { paddingTop: insets.top }]}>
-      <Text>This is main screen</Text>
+    <View style={[styles.container]}>
       <Carousel
         loop
         width={width}
-        height={width / 2}
+        height={Dimensions.get("screen").height}
+        style={{
+          width: width,
+          height: height,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
         autoPlay={true}
-        data={[...new Array(6).keys()]}
+        data={colors}
         scrollAnimationDuration={1000}
         onSnapToItem={(index) => console.log("current index:", index)}
-        renderItem={({ index }) => (
-          <View
-            style={{
-              flex: 1,
-              borderWidth: 1,
-              justifyContent: "center",
-            }}
-          >
-            <Text style={{ textAlign: "center", fontSize: 30 }}>{index}</Text>
-          </View>
+        customAnimation={animationStyle}
+        renderItem={({ index, item, animationValue }) => (
+          <CustomItem
+            key={index}
+            color={item}
+            animationValue={animationValue}
+          />
         )}
       />
     </View>
   );
 }
 
+interface ItemProps {
+  color: string;
+  animationValue: Animated.SharedValue<number>;
+}
+const CustomItem: React.FC<ItemProps> = ({ color, animationValue }) => {
+  const maskStyle = useAnimatedStyle(() => {
+    const backgroundColor = interpolateColor(
+      animationValue.value,
+      [-1, 0, 1],
+      ["#000000dd", "transparent", "#000000dd"]
+    );
+
+    return {
+      backgroundColor,
+    };
+  }, [animationValue]);
+
+  return (
+    <View style={{ flex: 1 }}>
+      {/* <SBItem pretty key={index} index={index} style={{ borderRadius: 0 }} /> */}
+      <View style={{ flex: 1, backgroundColor: color }}></View>
+      <Animated.View
+        pointerEvents="none"
+        style={[
+          {
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          },
+          maskStyle,
+        ]}
+      />
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors[0],
+    backgroundColor: "#000",
   },
 });
